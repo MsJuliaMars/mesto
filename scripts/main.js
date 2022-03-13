42bf4874e1f61139db37ca147be094cf12a67341
MsJuliaMars <izymka@list.ru>
2022-03-13 11:58:31 +0300

    v2.1

--- scripts/main.js
+++ scripts/main.js
@@ -1,35 +1,27 @@
-let popup = document.querySelector('.popup');
-let openPopup = document.querySelector('.profile__edit-button');
-let closePopup = popup.querySelector('.popup__close-button');
-let savePopup = popup.querySelector('.popup__save-button');
+let popup = document.querySelector(".popup");
+let openPopup = document.querySelector(".profile__edit-button");
+let closePopup = popup.querySelector(".popup__close-button");
+let formElement = document.querySelector(".popup__form");
+let nameInput = document.querySelector(".popup__text_name");
+let jobInput = document.querySelector(".popup__text_subtitle");
+let nameProfile = document.querySelector(".profile__title");
+let jobProfile = document.querySelector(".profile__subtitle");
+let saveButton = document.querySelector(".popup__save-button");
 
-function togglePopup() {
-    popup.classList.toggle('popup__opened');
-};
-openPopup.addEventListener('click', togglePopup);
-closePopup.addEventListener('click', togglePopup);
+openPopup.addEventListener("click", function () {
+  popup.classList.add("popup__opened");
+});
+closePopup.addEventListener("click", function () {
+  popup.classList.remove("popup__opened");
+});
+saveButton.addEventListener("click", function () {
+  popup.classList.remove("popup__opened");
+});
 
-
-let formElement = document.querySelector('.popup__form');
-
-let nameInput = document.querySelector('.popup__name'); 
-let jobInput = document.querySelector('.popup__subtitle'); 
-let nameProfile = document.querySelector('.profile__title');
-let jobProfile = document.querySelector('.profile__subtitle');
-
-function formSubmitHandler (evt) {
-    evt.preventDefault(); 
-    nameProfile.textContent = nameInput.value;
-    jobProfile.textContent = jobInput.value;
-
+function formSubmitHandler(evt) {
+  evt.preventDefault();
+  nameProfile.textContent = nameInput.value;
+  jobProfile.textContent = jobInput.value;
 }
-formElement.addEventListener('submit', formSubmitHandler); 
-savePopup.addEventListener('click', togglePopup);
 
-let like = document.querySelector('.card__like-button');
-function showLikeClick() {
-    like.classList.toggle('active');
-  }
-
-like.addEventListener('click', showLikeClick);
-
+formElement.addEventListener("submit", formSubmitHandler);
