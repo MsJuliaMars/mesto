25ef7a0707748d1e49b819c52ddea449d5f94826
MsJuliaMars <izymka@list.ru>
2022-03-19 09:29:29 +0300

    v3.0

--- scripts/main.js
+++ scripts/main.js
@@ -2,26 +2,28 @@ let popup = document.querySelector(".popup");
 let openPopup = document.querySelector(".profile__edit-button");
 let closePopup = popup.querySelector(".popup__close-button");
 let formElement = document.querySelector(".popup__form");
-let nameInput = document.querySelector(".popup__text_name");
-let jobInput = document.querySelector(".popup__text_subtitle");
+let nameInput = document.getElementById("popup-name");
+let jobInput = document.getElementById("popup-subtitle");
 let nameProfile = document.querySelector(".profile__title");
 let jobProfile = document.querySelector(".profile__subtitle");
-let saveButton = document.querySelector(".popup__save-button");
 
-openPopup.addEventListener("click", function () {
+function close() {
+  popup.classList.remove("popup__opened");
+}
+
+function open() {
   popup.classList.add("popup__opened");
-});
-closePopup.addEventListener("click", function () {
-  popup.classList.remove("popup__opened");
-});
-saveButton.addEventListener("click", function () {
-  popup.classList.remove("popup__opened");
-});
+  nameInput.value = nameProfile.textContent;
+  jobInput.value = jobProfile.textContent;
+}
 
 function formSubmitHandler(evt) {
   evt.preventDefault();
   nameProfile.textContent = nameInput.value;
   jobProfile.textContent = jobInput.value;
+  close();
 }
 
+openPopup.addEventListener("click", open);
+closePopup.addEventListener("click", close);
 formElement.addEventListener("submit", formSubmitHandler);
